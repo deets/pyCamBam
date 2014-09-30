91a94e344d94a64ca694ae630b16cf745844c7f8
Diez B. Roggisch <deets@web.de>
2014-09-30 23:21:53 +0200

    Moved placement to it's module

--- cambam/__init__.py
+++ cambam/__init__.py
@@ -1,3 +1,4 @@
 from .util import BBox
 from .matrix import Matrix, quaternion
 from .base import CamBam
+from .placement import grid
--- tests/test_placement.py
+++ tests/test_placement.py
@@ -1,33 +1,5 @@
-from math import sqrt, ceil
-from cambam import BBox, Matrix
+from cambam import BBox, grid
 import unittest
-
-
-def grid(bboxes):
-    max_width = max(bbox.width for bbox in bboxes)
-    max_length = max(bbox.length for bbox in bboxes)
-    max_height = max(bbox.height for bbox in bboxes)
-    xcount = int(ceil(sqrt(len(bboxes))))
-    ycount, rest = divmod(len(bboxes), xcount)
-    if rest:
-        ycount += 1
-
-    centers = [((x + .5) * max_width, (y + .5) * max_length)
-               for x in xrange(xcount)
-               for y in xrange(ycount)]
-
-    res = []
-    for center, bbox in zip(centers, bboxes):
-        m = Matrix()
-        old = bbox.center
-        m.translate(x=center[0] - old[0], y=center[1] - old[1])
-        res.append(m)
-    stock = BBox(
-        (0, 0, 0),
-        (xcount * max_width, ycount * max_length, max_height),
-        )
-    return stock, res
-
 
 class TestPlacemont(unittest.TestCase):
 
